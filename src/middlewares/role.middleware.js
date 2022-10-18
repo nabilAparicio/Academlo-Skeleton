4f04947403d64baea1c1a487c0789318514ef640
orlando <orlando.nabil@gmail.com>
2022-10-18 16:11:09 -0500

    initial commit

---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,9 @@
+const adminValidate = (req, res, next) => {
+  const role = req.user.role;
+  if (role === "admin") {
+    return next();
+  }
+  return res.status(401).json({ message: "Access Deny!" });
+};
+
+module.exports = adminValidate;
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,17 +1,14 @@
-const router = require('express').Router()
-const passport = require('passport')
+const router = require("express").Router();
+const passport = require("passport");
+const adminValidate = require("../middlewares/role.middleware");
 
-const userServices = require('./users.services')
+const userServices = require("./users.services");
 
-require('../middlewares/auth.middleware')(passport)
-
-
-
-
+require("../middlewares/auth.middleware")(passport);
 
 //? rutas raiz
 
-router.get('/', userServices.getAllUsers)
+router.get("/", userServices.getAllUsers);
 
 //TODO el registerUser ira en la ruta /auth/register
 
@@ -25,21 +22,32 @@ router.get('/', userServices.getAllUsers)
 //! router.delete('/:id')
 
 //? Ruta de informacion propia del usuario loggeado
-router.route('/me')
-    .get(
-        passport.authenticate('jwt', {session: false}),
-        userServices.getMyUser)
-    //.patch()
-    //.delete()
+router
+  .route("/me")
+  .get(passport.authenticate("jwt", { session: false }), userServices.getMyUser)
+  .patch(
+    passport.authenticate("jwt", { session: false }),
+    userServices.patchMyUser
+  )
+
+  .delete(
+    passport.authenticate("jwt", { session: false }),
+    userServices.deleteMyUser
+  );
 
 //? /api/v1/users/:id
-router.route('/:id')
-    .get(userServices.getUserById)
-    .patch(userServices.patchUser)
-    .delete(userServices.deleteUser)
+router
+  .route("/:id")
+  .get(userServices.getUserById)
+  .patch(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    userServices.patchUser
+  )
+  .delete(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    userServices.deleteUser
+  );
 
-
-
-
-
-module.exports = router
+module.exports = router;
